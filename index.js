0a99ed5c698f00eb7932a5612d3fd6517448d7d0
Sheersho Pramanik <sheersho.pramanik@gmail.com>
2019-05-18 09:53:11 +0530

    Sheersho's World Login Page

---
+++ index.js
@@ -0,0 +1,27 @@
+console.log('Hello World');
+
+let name = 'Sheersho';
+console.log(name);
+
+alert('This is actaully working, Sheersho');
+
+
+function check() {
+    document.getElementById("myCheck").checked = true;
+  }
+  
+  function uncheck() {
+    document.getElementById("myCheck").checked = false;
+
+    function dblcheck()
+    {
+        document.getElementById("myCheck").checked = true
+    }
+  }
+
+  const name = "Sheersho";
+  const age =21;
+  const rating = 4.81;
+  const isCool = true;
+
+  console.log(typeof name);
